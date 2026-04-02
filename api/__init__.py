cc8087d50466f779fe5f607658736c1920f711fa
kalil0321 <kalil.bouzigues@gmail.com>
2026-04-02 19:33:33 +0200

    Clean up: update gitignore, remove deprecated api/ module and obsolete files

--- api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Job Data API Package
-"""
-from .main import app
-
-__version__ = "1.0.0"
-__all__ = ["app"]
--- api/providers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Job data providers package
-"""
-from .base import BaseJobProvider
-from .serpapi import SerpAPIProvider
-from .jsearch import JSearchProvider
-
-__all__ = ["BaseJobProvider", "SerpAPIProvider", "JSearchProvider"]
